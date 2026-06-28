ed23c67fa908a433ca91eae145b8783ec3282ff1
rahulmalakar42 <malakarahul111@gmail.com>
2026-06-28 13:49:51 +0530

    c done

--- recursion/C_Print_from_N_to_1.cpp
+++ recursion/C_Print_from_N_to_1.cpp
@@ -4,13 +4,21 @@ using namespace std;
 
 #define fast_io ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
 
+void recurse(int t){
+    if(!t)return;
+
+    if(t==1) cout<<t;
+    else cout<<t<<" ";
+    recurse(t-1);
+}
+
 int main(){
 
     fast_io;
 
     int t; cin>>t;
 
-    
+    recurse(t);
     
     return 0;
 }
